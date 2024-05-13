bf961a60ba73a6feb2d58726a6c256de38ee7817
KvaZar <razumniydenis@gmail.com>
2024-05-13 17:33:46 +0300

    Базовые арефметические операторы

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 
+/*
 let a = 1;
 console.log(a);
 
 a = 'test'
 console.log(a);
+*/
+
+// Базовые арефметические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space);
+
+//Строки
+const city = 'Николаев';
+const street = 'Водопроводная';
+console.log(city + ', ' + street + ' ' + 5);
